34d7db12a81f0cde018c48b1163aa1c374988cd9
AlexanderCortez <joelalexander10@outlook.com>
2021-09-26 01:50:16 -0600

    ✨ Indicator component

--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -2,3 +2,4 @@ export * from 'types/components/Card';
 export * from 'types/components/Icon';
 export * from 'types/components/Layout';
 export * from 'types/components/Descriptions';
+export * from 'types/components/Indicator';
